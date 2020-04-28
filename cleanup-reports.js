f575ae16477f8cfd0fd758d584cdd3613b3bb4d8
Huy Nguyen <huynguyen@qasymphony.com>
2020-04-28 14:25:37 -0400

    added cleanup-reports.js

---
+++ cleanup-reports.js
@@ -0,0 +1,14 @@
+const { execSync } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+const isWin = process.platform == "win32";
+const workingDir = process.cwd();
+const reportDir = path.resolve(workingDir, 'results');
+
+if (fs.existsSync(reportDir)) {
+  let deleteCommand = isWin ? `rmdir /s /q "${reportDir}"` : `rm -rf "${reportDir}"`;
+  execSync(deleteCommand, { 
+    cwd: workingDir, 
+    stdio: 'inherit' 
+  });
+}
