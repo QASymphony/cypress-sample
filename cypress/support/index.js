8ccfe6579a88e69bf782680f42a95ed1805f888b
Huy Nguyen <huynguyen@qasymphony.com>
2020-05-01 11:03:39 -0400

    execute selected tests

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -18,3 +18,43 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+/** 
+ * Global before hook that runs before every single test is being executed. What it does: 
+ * - Checks whether specific test name (or names) are specified in `cypress run --env tests='<array of test names>'` command
+ * - If there is/are test name(s) specified, skip the current test if its name is not included in the test name list
+ * - If there is no test name specified in --env, the test will be run normally
+*/
+beforeEach(function() {
+  var getCurrenExecutingtTestName = (currentTest) => {
+    let testTitles = [];
+    var extractTitles = (obj) => {
+      if (obj.hasOwnProperty('parent')) {
+        testTitles.push(obj.title);
+        let nextObj = obj.parent;
+        extractTitles(nextObj);
+      }
+    }
+    extractTitles(currentTest);
+    let orderedTitles = testTitles.reverse();
+    let testName = orderedTitles.join(' ').trim();
+    return testName;
+  }
+
+  let currenExecutingtTestName = getCurrenExecutingtTestName(this.currentTest);
+  // Obtain test names in --env parameter. Refer to the script cypress:run:selected-tests-only in package.json to know how to sepecify specific tests to run
+  // values are either empty if not specified in cypress run command, or and array of test name, like so:
+  //    ["<context name> <title 1>","<context name> <describe name> <title 2>","<describe name> <describe name> <title n>", etc. etc.]' 
+  let desiredTestNamesToBeRun = Cypress.env("tests");
+  if (desiredTestNamesToBeRun != undefined && Array.isArray(desiredTestNamesToBeRun)) {
+    // if specific test names are specified in cypress run --env tests='[<test names>]' command, 
+    // skip this test if its name is *not* included in the test name list
+    if (!desiredTestNamesToBeRun.includes(currenExecutingtTestName)) {
+      cy.log(`Skip test '${currenExecutingtTestName}' as it's not included in 'tests' env.`);
+      cy.state('runnable').ctx.skip();
+    }
+  }
+  else {
+    cy.log(`No specific test being specified in cypress run --env command, this test will be run normally.`);
+  }
+});
